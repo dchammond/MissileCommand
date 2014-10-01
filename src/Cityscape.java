fff80a37f999c31b6218c95e2417af02063eddb3
icehockey38 <dillonhammond@gmail.com>
2014-10-01 13:00:51 -0500

    Started writing viewer code

--- src/Cityscape.java
+++ src/Cityscape.java
@@ -1,12 +1,5 @@
-import javax.swing.JFrame;
-import java.awt.Color;
 
 public class Cityscape {
 
-	private static int numOfBases;
 
-
-	public Cityscape() {
-
-	}
 }
--- src/CityscapeViewer.java
+++ src/CityscapeViewer.java
@@ -1,5 +1,37 @@
+import javax.swing.JFrame;
+import java.awt.Color;
+
 /**
  * Created by Dillon on 10/1/14.
  */
 public class CityscapeViewer {
+
+
+	private static int numOfBases;
+	private static Color groundColor;
+	private static int[] color;
+
+	public CityscapeViewer(int[] color) {
+		this.numOfBases = 6;
+		this.color = color;
+		this.groundColor = new Color(color[0], color[1], color[2]);
+	}
+
+	public static void main() {
+		JFrame frame = new JFrame();
+
+		frame.setSize(800, 600);
+		frame.setTitle("Missile Command");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		CityscapeComponent component = new CityscapeComponent();
+
+		frame.add(component);
+
+		frame.setVisible(true);
+
+		while (true) {
+			component.repaint();
+		}
+	}
 }
