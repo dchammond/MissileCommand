a41d5f7f717d2d47fcf66bd09637bd380be68ad1
icehockey38 <dillonhammond@gmail.com>
2014-10-04 14:39:46 -0500

    Removed .class from track

--- src/com/DillonHammond/MissileCommand/Cityscape.java
+++ src/com/DillonHammond/MissileCommand/Cityscape.java
@@ -1,21 +1,13 @@
 package com.DillonHammond.MissileCommand;
 
 import java.awt.Graphics2D;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Line2D;
-import java.awt.geom.Point2D;
-import java.awt.Color;
 
 public class Cityscape {
 
-	private static int numOfBases;
-	private static Color groundColor;
 	private static int width;
 	private static int height;
 
-	public Cityscape(int width, int height, int numOfBases, Color groundColor) {
-		this.numOfBases = numOfBases;
-		this.groundColor = groundColor;
+	public Cityscape(int width, int height) {
 		this.width = width;
 		this.height = height;
 	}
