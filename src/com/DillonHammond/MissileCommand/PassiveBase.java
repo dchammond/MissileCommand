035b7a958407f62e8d82f6376e4b27f1f5f7bde9
icehockey38 <dillonhammond@gmail.com>
2014-10-04 22:13:37 -0500

    Added cities to the constructor

--- src/com/DillonHammond/MissileCommand/PassiveBase.java
+++ src/com/DillonHammond/MissileCommand/PassiveBase.java
@@ -3,6 +3,7 @@ package com.DillonHammond.MissileCommand;
 import java.awt.geom.Rectangle2D;
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.geom.Arc2D;
 
 /**
  * Created by Dillon on 10/4/14.
@@ -16,19 +17,34 @@ public class PassiveBase {
 	private static double baseHeight;
 	private static double screenWidth;
 	private static double screenHeight;
+	private static Color cityColor;
+	private static double cityPosX;
+	private static double cityPosY;
+	private static double cityWidth;
+	private static double cityHeight;
+	private static final double ANGLE = 180.0;
 
-	public PassiveBase(Color baseColor, int screenWidth, int screenHeight) {
-		this.baseColor = baseColor;
+	public PassiveBase(Color baseColor, Color cityColor, int screenWidth, int screenHeight) {
+		// Get screen values
 		this.screenWidth = (double) screenWidth;
 		this.screenHeight = (double) screenHeight;
-		this.basePosX = (this.screenWidth / 9.0);
-		this.basePosY = (this.screenHeight / 6.0);
+		// Start base creation
+		this.baseColor = baseColor;
+		this.basePosX = (this.screenWidth / 9.0); // Each base is a 9th of the screen across
+		this.basePosY = (this.screenHeight / 6.0); // Each base is a 6th of the screnn in height
 		this.baseWidth = this.basePosX; // This is just for better name clarification
 		this.baseHeight = this.basePosY; // This is just for better name clarification
+		// Start city creation
+		this.cityColor = cityColor;
+		this.cityPosX = this.basePosX; // X coord of a city's rect frame is same as its base
+		this.cityPosY = this.basePosY - (this.basePosY/2.0); // Top of a city is above the base by half of the base's height
+		this.cityWidth = this.cityPosX; // For better readability
+		this.cityHeight = (this.basePosY/2.0); // Height of a city is half that of the base
 	}
 
 	public static Rectangle2D.Double[] makeBases() {
 		// Rectangle dimensions are always 1/9 X 1/6 of the screen size
+		// Rectangles are placed at 5/6 of the screen's total height
 		// Rectangle locations are as follows: edge-->|T|x|x|x|T|x|x|x|T|<--edge (x is rect being drawn below, T is turret)
 		Rectangle2D.Double[] listOfBases = {
 		   new Rectangle2D.Double(1.0 * basePosX, 5.0 * basePosY, baseWidth, baseHeight),
