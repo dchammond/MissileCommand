5eb4c946ac80fec43df6719ddb0091721288fbdb
icehockey38 <dillonhammond@gmail.com>
2014-10-04 23:04:35 -0500

    Added drawing of turret bases

--- src/com/DillonHammond/MissileCommand/PassiveBase.java
+++ src/com/DillonHammond/MissileCommand/PassiveBase.java
@@ -32,14 +32,17 @@ public class PassiveBase {
 	public static Rectangle2D.Double[] makeBases() {
 		// Rectangle dimensions are always 1/9 X 1/6 of the screen size
 		// Rectangles are placed at 5/6 of the screen's total height
-		// Rectangle locations are as follows: edge-->|T|x|x|x|T|x|x|x|T|<--edge (x is rect being drawn below, T is turret)
+		// Rectangle locations are as follows: edge-->|T|x|x|x|T|x|x|x|T|<--edge (x is rect being drawn below, T is turret being drawn)
 		Rectangle2D.Double[] listOfBases = {
+		   new Rectangle2D.Double(0.0 * basePosX, (5.0 * basePosY) - 50.0, baseWidth, (baseHeight + 50.0)),
 		   new Rectangle2D.Double(1.0 * basePosX, 5.0 * basePosY, baseWidth, baseHeight),
 		   new Rectangle2D.Double(2.0 * basePosX, 5.0 * basePosY, baseWidth, baseHeight),
 		   new Rectangle2D.Double(3.0 * basePosX, 5.0 * basePosY, baseWidth, baseHeight),
+		   new Rectangle2D.Double(4.0 * basePosX, (5.0 * basePosY) - 50.0, baseWidth, (baseHeight + 50.0)),
 		   new Rectangle2D.Double(5.0 * basePosX, 5.0 * basePosY, baseWidth, baseHeight),
 		   new Rectangle2D.Double(6.0 * basePosX, 5.0 * basePosY, baseWidth, baseHeight),
-		   new Rectangle2D.Double(7.0 * basePosX, 5.0 * basePosY, baseWidth, baseHeight)
+		   new Rectangle2D.Double(7.0 * basePosX, 5.0 * basePosY, baseWidth, baseHeight),
+		   new Rectangle2D.Double(8.0 * basePosX, (5.0 * basePosY) - 50.0, baseWidth, (baseHeight + 50.0)),
 		};
 		return listOfBases;
 	}
@@ -48,7 +51,7 @@ public class PassiveBase {
 		Rectangle2D.Double[] allTheBases = makeBases();
 
 		g2.setColor(baseColor);
-		for (int i = 0; i < 6; i++) {
+		for (int i = 0; i < 9; i++) {
 			g2.draw(allTheBases[i]);
 			g2.fill(allTheBases[i]);
 		}
