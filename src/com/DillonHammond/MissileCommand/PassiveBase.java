17a6ca4ac6478c54b42ac977bab5ffe1472f2a6a
icehockey38 <dillonhammond@gmail.com>
2014-10-04 22:59:48 -0500

    Moved cities to another class and implemented

--- src/com/DillonHammond/MissileCommand/PassiveBase.java
+++ src/com/DillonHammond/MissileCommand/PassiveBase.java
@@ -3,7 +3,6 @@ package com.DillonHammond.MissileCommand;
 import java.awt.geom.Rectangle2D;
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.geom.Arc2D;
 
 /**
  * Created by Dillon on 10/4/14.
@@ -17,14 +16,8 @@ public class PassiveBase {
 	private static double baseHeight;
 	private static double screenWidth;
 	private static double screenHeight;
-	private static Color cityColor;
-	private static double cityPosX;
-	private static double cityPosY;
-	private static double cityWidth;
-	private static double cityHeight;
-	private static final double ANGLE = -180.0;
 
-	public PassiveBase(Color baseColor, Color cityColor, int screenWidth, int screenHeight) {
+	public PassiveBase(Color baseColor, int screenWidth, int screenHeight) {
 		// Get screen values
 		this.screenWidth = (double) screenWidth;
 		this.screenHeight = (double) screenHeight;
@@ -34,12 +27,6 @@ public class PassiveBase {
 		this.basePosY = (this.screenHeight / 6.0); // Each base is a 6th of the screnn in height
 		this.baseWidth = this.basePosX; // This is just for better name clarification
 		this.baseHeight = this.basePosY; // This is just for better name clarification
-		// Start city creation
-		this.cityColor = cityColor;
-		this.cityPosX = this.basePosX; // X coord of a city's rect frame is same as its base
-		this.cityPosY = (5.0*this.basePosY) - 35.0; // Top of a city is above the base by 35px (this is due to the screen menu bar taking up some pixels)
-		this.cityWidth = this.baseWidth; // For better readability
-		this.cityHeight = (this.basePosY/2.0); // Height of a city is half that of the base
 	}
 
 	public static Rectangle2D.Double[] makeBases() {
@@ -57,36 +44,13 @@ public class PassiveBase {
 		return listOfBases;
 	}
 
-	public static Arc2D.Double[] makeCities() {
-		// City dimensions are in a rect that is the same width as a base but half as tall
-		// City positions are multiplied to match the base they rest upon
-		// ANGLE is -180.0 because this draws a hemisphere
-		// Arc2D.CHORD allows the hemisphere's bottom to be a straight line
-		Arc2D.Double[] listOfCities = {
-		   new Arc2D.Double(1.0*cityPosX, cityPosY, cityWidth, cityHeight, ANGLE, ANGLE, Arc2D.CHORD),
-		   new Arc2D.Double(2.0*cityPosX, cityPosY, cityWidth, cityHeight, ANGLE, ANGLE, Arc2D.CHORD),
-		   new Arc2D.Double(3.0*cityPosX, cityPosY, cityWidth, cityHeight, ANGLE, ANGLE, Arc2D.CHORD),
-		   new Arc2D.Double(5.0*cityPosX, cityPosY, cityWidth, cityHeight, ANGLE, ANGLE, Arc2D.CHORD),
-		   new Arc2D.Double(6.0*cityPosX, cityPosY, cityWidth, cityHeight, ANGLE, ANGLE, Arc2D.CHORD),
-		   new Arc2D.Double(7.0*cityPosX, cityPosY, cityWidth, cityHeight, ANGLE, ANGLE, Arc2D.CHORD)
-		};
-		return listOfCities;
-	}
-
 	public static void draw(Graphics2D g2) {
 		Rectangle2D.Double[] allTheBases = makeBases();
-		Arc2D.Double[] allTheCities = makeCities();
 
 		g2.setColor(baseColor);
 		for (int i = 0; i < 6; i++) {
 			g2.draw(allTheBases[i]);
 			g2.fill(allTheBases[i]);
-		}
-
-		g2.setColor(cityColor);
-		for (int i = 0; i < 6; i++) {
-			g2.draw(allTheCities[i]);
-			g2.fill(allTheCities[i]);
 		}
 	}
 }
