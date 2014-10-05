2f1f13ad2e4819bfa5a9a5033b6073636f8043bf
icehockey38 <dillonhammond@gmail.com>
2014-10-04 21:42:21 -0500

    Properly created all of the rectangles

--- src/com/DillonHammond/MissileCommand/PassiveBase.java
+++ src/com/DillonHammond/MissileCommand/PassiveBase.java
@@ -2,36 +2,45 @@ package com.DillonHammond.MissileCommand;
 
 import java.awt.geom.Rectangle2D;
 import java.awt.Color;
+import java.awt.Graphics2D;
 
 /**
  * Created by Dillon on 10/4/14.
  */
 public class PassiveBase {
 
-	private static int numOfBases;
 	private static Color baseColor;
+	private static double basePosX;
+	private static double basePosY;
 	private static double baseWidth;
 	private static double baseHeight;
 	private static double screenWidth;
 	private static double screenHeight;
 
 	public PassiveBase(Color baseColor, int screenWidth, int screenHeight) {
-		this.numOfBases = 6;
 		this.baseColor = baseColor;
 		this.screenWidth = (double) screenWidth;
 		this.screenHeight = (double) screenHeight;
-		this.baseWidth = (this.screenWidth / 6.0);
-		this.baseHeight = (this.screenHeight / 6.0);
+		this.basePosX = (this.screenWidth / 9.0);
+		this.basePosY = (this.screenHeight / 6.0);
+		this.baseWidth = this.basePosX;
+		this.baseHeight = this.basePosY;
 	}
 
 	public static Rectangle2D.Double[] makeBases() {
-		Rectangle2D.Double[] listOfBases = (new Rectangle2D.Double(100d, 500d, 100d, 100d),
-											new Rectangle2D.Double(200d, 500d, 100d, 100d),
-											new Rectangle2D.Double(300d, 500d, 100d, 100d),
-											new Rectangle2D.Double(500d, 500d, 100d, 100d),
-											new Rectangle2D.Double(600d, 500d, 100d, 100d),
-											new Rectangle2D.Double(700d, 500d, 100d, 100d)
+		// Rectangle dimensions are always 1/9 X 1/6 of the screen size
+		// Rectangle locations are as follows: edge-->|T|x|x|x|T|x|x|x|T|<--edge (x is rect being drawn below, T is turret)
+		Rectangle2D.Double[] listOfBases = (new Rectangle2D.Double(1.0*basePosX, 5.0*basePosY, baseWidth, baseHeight),
+											new Rectangle2D.Double(2.0*basePosX, 5.0*basePosY, baseWidth, baseHeight),
+											new Rectangle2D.Double(3.0*basePosX, 5.0*basePosY, baseWidth, baseHeight),
+											new Rectangle2D.Double(5.0*basePosX, 5.0*basePosY, baseWidth, baseHeight),
+											new Rectangle2D.Double(6.0*basePosX, 5.0*basePosY, baseWidth, baseHeight),
+											new Rectangle2D.Double(7.0*basePosX, 5.0*basePosY, baseWidth, baseHeight)
 											);
 		return listOfBases;
+	}
+
+	public static void draw(Graphics2D g2) {
+
 	}
 }
