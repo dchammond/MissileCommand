39f65cee16e1c8059d6550e7b8b158fe32fcaae5
icehockey38 <dillonhammond@gmail.com>
2014-10-05 01:52:08 -0500

    Draws all 3 turrets

--- src/com/DillonHammond/MissileCommand/ActiveTurret.java
+++ src/com/DillonHammond/MissileCommand/ActiveTurret.java
@@ -12,10 +12,9 @@ public class ActiveTurret {
 	private static Color turretColor;
 	private static double screenWidth;
 	private static double screenHeight;
-	private static double numOfLines;
 	private static double turretHeight;
 	private static double turretWidth;
-	private static Trapezoid turrets;
+//	private static Trapezoid turret;
 
 	public ActiveTurret(Color turretColor, double screenWidth, double screenHeight) {
 		// Get screen values
@@ -25,11 +24,14 @@ public class ActiveTurret {
 		this.turretColor = turretColor;
 		this.turretHeight = ((this.screenHeight/6.0)/2.0); // The turret will be half of the base height (screenHeight/6.0)
 		this.turretWidth = (this.screenWidth/9.0); // The turret will be as wide as the base
-		this.numOfLines = (int) this.turretHeight; // We can't have .2 lines, there are as many lines as the turret height)
-		this.turrets = new Trapezoid(Color.YELLOW, this.turretWidth, (5.0*(this.screenHeight/6.0))-50.0, turretHeight, (0.0*turretWidth));
 	}
 
 	public static void draw(Graphics2D g2) {
-		turrets.makeTrap(g2);
+		double x = 0.0;
+		for (int i = 0; i < 3; i++) {
+			Trapezoid turret = new Trapezoid(turretColor, turretWidth, (5.0*(screenHeight/6.0))-50.0, turretHeight, (x*turretWidth));
+			turret.makeTrap(g2);
+			x += 4.0;
+		}
 	}
 }
--- src/com/DillonHammond/MissileCommand/Trapezoid.java
+++ src/com/DillonHammond/MissileCommand/Trapezoid.java
@@ -3,7 +3,6 @@ package com.DillonHammond.MissileCommand;
 import java.awt.geom.Line2D;
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.util.ArrayList;
 
 /**
  * Created by Dillon on 10/4/14.
@@ -16,7 +15,6 @@ public class Trapezoid {
 	private static double trapBaseWidth;
 	private static double trapBaseY;
 	private static double lineStartPosX;
-	private static ArrayList<Line2D.Double> lines = new ArrayList<Line2D.Double>();
 
 	public Trapezoid(Color lineColor, double trapBaseWidth, double trapBaseY, double trapHeight, double lineStartPosX) {
 		this.lineColor = lineColor;
@@ -31,13 +29,13 @@ public class Trapezoid {
 		g2.setColor(lineColor);
 		for (int i = 0; i < numOfLines; i++) {
 			double x1 = lineStartPosX + ((i*(trapBaseWidth/3.0))/50.0);
-			double x2 = trapBaseWidth - ((i*(trapBaseWidth/3.0))/50.0);
+			double x2 = lineStartPosX + (trapBaseWidth - ((i*(trapBaseWidth/3.0))/50.0));
 			double y1 = trapBaseY - (i);
 			double y2 = y1; // Always a straight line
 
-			lines.add(new Line2D.Double(x1, y1, x2, y2));
-			g2.draw(lines.get(i));
-			g2.fill(lines.get(i));
+			Line2D.Double line = new Line2D.Double(x1, y1, x2, y2);
+			g2.draw(line);
+			g2.fill(line);
 		}
 	}
 }
