5133787dbcfc19a1c800e6fa66617e6a845fecfa
icehockey38 <dillonhammond@gmail.com>
2014-10-04 22:46:23 -0500

    Added instance variables

--- src/com/DillonHammond/MissileCommand/PassiveCity.java
+++ src/com/DillonHammond/MissileCommand/PassiveCity.java
@@ -9,4 +9,14 @@ import java.awt.geom.Arc2D;
  * Created by Dillon on 10/4/14.
  */
 public class PassiveCity {
+
+	private static double screenWidth;
+	private static double screenHeight;
+	private static Color cityColor;
+	private static double cityPosX;
+	private static double cityPosY;
+	private static double cityWidth;
+	private static double cityHeight;
+	private static final double ANGLE = -180.0;
+
 }
