163b1a9a079ac12c725572dac7e8ea2d8f3f6499
icehockey38 <dillonhammond@gmail.com>
2014-10-07 23:00:25 -0500

    Removed all bomb stuff

--- src/com/DillonHammond/MissileCommand/Cityscape.java
+++ src/com/DillonHammond/MissileCommand/Cityscape.java
@@ -13,9 +13,6 @@ public class Cityscape {
 	private static PassiveCity passiveCities;
 	private static ActiveTurret turret;
 	private static Road road;
-	private static Color bombColor;
-	private static Bomb bomb;
-	private static int x = 0;
 
 	public Cityscape(int width, int height) {
 		this.width = width;
@@ -26,8 +23,6 @@ public class Cityscape {
 		this.passiveCities = new PassiveCity(this.cityColor, this.width, this.height);
 		this.turret = new ActiveTurret(Color.BLUE, this.width, this.height);
 		this.road = new Road(this.width, this.height);
-		this.bombColor = new Color(0, 255, 0);
-		this.bomb = new Bomb(this.bombColor, this.width, this.height);
 	}
 
 	public void draw(Graphics2D g2) {
@@ -36,11 +31,6 @@ public class Cityscape {
 		turret.draw(g2);
 		road.draw(g2);
 		turret.activateTurret();
-		if (x==0) {
-			bomb.makeTimer();
-			x++;
-		}
-		bomb.draw(g2);
 	}
 
 }
