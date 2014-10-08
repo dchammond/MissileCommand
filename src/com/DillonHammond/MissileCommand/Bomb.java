20cdd97a4c699204cc0c89c0764b8cead7797c7d
icehockey38 <dillonhammond@gmail.com>
2014-10-07 20:20:31 -0500

    Made some changes

--- src/com/DillonHammond/MissileCommand/Bomb.java
+++ src/com/DillonHammond/MissileCommand/Bomb.java
@@ -20,7 +20,8 @@ public class Bomb {
 	private static double screenHeight;
 	private static double[] validBombCreation = new double[]{0.0,0.0};
 	private static double[] origin = new double[]{0.0,0.0};
-	private static double[] end = new double[]{0.0,0.0};
+	private static ArrayList<Double> endX = new ArrayList<Double>();
+	private static ArrayList<Double> endY = new ArrayList<Double>();
 
 	public Bomb(Color bombColor, double screenWidth, double screenHeight) {
 		this.bombColor = bombColor;
@@ -31,8 +32,6 @@ public class Bomb {
 	}
 
 	public static ArrayList<Line2D.Double> makeBombs(double[] amountDraw) {
-		System.out.println(amountDraw[0]);
-		System.out.println(amountDraw[1]);
 		bombs.add(new Line2D.Double(origin[0], origin[1], amountDraw[0], amountDraw[1]));
 
 		return bombs;
@@ -50,16 +49,20 @@ public class Bomb {
 		if (2.0*validBombCreation[0]/random >= screenWidth) {
 			origin[0] = screenWidth - 10.0;
 		}
-		end[0] = PassiveCity.listOfCities.get(randomTurret).getX() + screenWidth/18.0;
-		end[1] = PassiveCity.listOfCities.get(randomTurret).getY() + 10.0;
+		double finalEndX = PassiveCity.listOfCities.get(randomTurret).getX() + screenWidth/18.0;
+		double finalEndY = PassiveCity.listOfCities.get(randomTurret).getY() + 10.0;
+		for (double i = 0.0; i <=1.0 ; i += 0.1) {
+			endX.add(finalEndX*i);
+			endY.add(finalEndY*i);
+		}
 	}
 
 	public static void draw(Graphics2D g2) {
 		double percentDraw = 0.0;
 		generateRandomValues();
 
-		for (percentDraw = 1.0; percentDraw > 0.0; percentDraw -= 0.01) {
-			double[] amountDraw = {end[0] - percentDraw * end[0], end[1] - percentDraw * end[1]};
+		for (int i = 0; i < endX.size(); i++) {
+			double[] amountDraw = {endX.get(i), endY.get(i)};
 			bombs = makeBombs(amountDraw);
 		}
 
